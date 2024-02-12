f294b472df76f63e45b58f64320319ba73ded7e7
Alexey <temnikovalx@icloud.com>
2024-02-12 12:10:37 +0300

    fix problems

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -62,4 +62,6 @@ export const authOptions: NextAuthOptions = {
 		error: "/auth",
 	},
 };
-export default NextAuth(authOptions);
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
