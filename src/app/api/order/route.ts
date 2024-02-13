c529ba2d31f7531118249779fa5e48ae1ed04317
Alexey <temnikovalx@icloud.com>
2024-02-13 13:20:16 +0300

    check for dat

--- src/app/api/order/route.ts
+++ src/app/api/order/route.ts
@@ -12,7 +12,22 @@ export async function DELETE(req: Request, route: { params: { id: string } }) {
 }
 export async function POST(req: Request, route: { params: { id: string } }) {
     try {
-      return NextResponse.json({ comments: {}}, { status: 200 });
+      const body = await req.json();
+      const { email, id, name}  = body;
+        const studentsRef = doc(db, "students", id);
+                const docSnap = await getDoc(studentsRef);
+                if (docSnap.exists()) {
+                  console.log("Document data:", docSnap.data());
+                  return NextResponse.json({data: docSnap.data() }, { status: 200 });
+                  } else {
+                  await setDoc(studentsRef, {
+                    id: id,
+                    name: name,
+                    email: email,
+                    accepted: false,
+                    });
+                  }
+      return NextResponse.json({ data: 'created'}, { status: 200 });
     } catch (err) {
       console.error(err);
       return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -4,29 +4,42 @@ import AuthForm from "./AuthForm";
 import SignInButton from "./AuthTelegram";
 import { fetcher } from "@/lib/fetcher";
 import useSWR from "swr";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function AuthPage() {
     const session = useSession();
-    const { data, isLoading, error } = useSWR(`/api/order?id=${session.data?.user.id}`, fetcher)
+    const [data, setData] = useState()
+    const sendOrCheckUser = async () => {
+        await axios.post('/api/order', {
+            id: session.data?.user.id,
+            name: session.data?.user.name,
+            email: session.data?.user.email
+        }).then((res: any) => setData(res))
+    }
+    useEffect(() => {
+        session.status == 'authenticated' && sendOrCheckUser()
+    }, [session.status])
 
-    if (session)
-        return (
-            <main className="flex min-h-screen items-center justify-center bg-accentViolet">
-                <div className="max-w-[1000px] flex gap-[150px] items-center max-[1000px]:flex-col max-[1000px]:gap-[30px]">
-                    <div className={"flex flex-col gap-[20px] items-center"}>
-                        <div className={"text-[60px] text-black font-semibold px-[40px] py-[35px] bg-white rounded-[20px] max-[1000px]:text-[36px] max-[1000px]:px-[20px] max-[1000px]:py-[10px]"} >
-                            Практика.ру
+    if (session.status)
+        if (session)
+            return (
+                <main className="flex min-h-screen items-center justify-center bg-accentViolet">
+                    <div className="max-w-[1000px] flex gap-[150px] items-center max-[1000px]:flex-col max-[1000px]:gap-[30px]">
+                        <div className={"flex flex-col gap-[20px] items-center"}>
+                            <div className={"text-[60px] text-black font-semibold px-[40px] py-[35px] bg-white rounded-[20px] max-[1000px]:text-[36px] max-[1000px]:px-[20px] max-[1000px]:py-[10px]"} >
+                                Практика.ру
+                            </div>
+                            <p className={"text-white text-[48px] font-semibold max-[1000px]:text-[24px] "}>Задания здесь!</p>
                         </div>
-                        <p className={"text-white text-[48px] font-semibold max-[1000px]:text-[24px] "}>Задания здесь!</p>
+                        <div>
+                            {JSON.stringify(data)}
+                        </div>
+                        {/* {session.} */}
+                        {/* <AuthForm /> */}
+                        <SignInButton botUsername={`${process.env.BOT_USERNAME}`} />
                     </div>
-                    <div>
-                        {!isLoading && JSON.stringify(data)}
-                    </div>
-                    {/* {session.} */}
-                    {/* <AuthForm /> */}
-                    <SignInButton botUsername={`${process.env.BOT_USERNAME}`} />
-                </div>
-            </main>
-        );
+                </main>
+            );
 }
 
