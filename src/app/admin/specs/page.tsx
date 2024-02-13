f05f16b33530e553d2dfac3078867eda25e5339a
Mikhail Pshenichnyy <dremast1337@gmail.com>
2024-02-13 10:37:34 +0300

    queries for api done for specialization

--- src/app/admin/specs/page.tsx
+++ src/app/admin/specs/page.tsx
@@ -6,55 +6,130 @@ import { Label } from "@/components/ui/label";
 import { DataTable } from "@/lib/DataTable/DataTable"
 import { ColumnDef } from "@tanstack/react-table"
 import { MoreHorizontal } from "lucide-react";
+import useSWR, {useSWRConfig} from "swr";
+import useModal from "@/lib/useModal";
+import {useState} from "react";
+import {fetcher} from "@/lib/fetcher";
+import axios from "axios";
+import {Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import Modal from "@/components/ui/modal";
 
 type Spec = {
-    id: string | number;
-    Spec: string;
+    id: string;
+    name: string;
 };
-const columns: ColumnDef<Spec>[] = [
-    {
-        header: "ID",
-        accessorKey: "id",
-    },
-    {
-        header: "Специальность",
-        accessorKey: "Spec",
-    },
-    {
-        id: "actions",
-        cell: ({ row }) => {
-            const order = row.original
-            return (
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Действия</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>Удалить</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            )
-        },
-    },
-];
-async function getData(): Promise<Spec[]> {
-    return [
+
+
+export default function SpecsPage() {
+
+    // mutate - для того чтобы сделать refresh в получении data по запросу
+    const { mutate } = useSWRConfig();
+
+    // мой кастомный хук для работы с модалкой
+    const { isModalOpen, openModal, closeModal } = useModal();
+
+    // input для добавления вуза
+    const [inputValue, setInputValue] = useState('')
+
+    // это по сути input в модалке
+    const [newValue, setNewValue] = useState('')
+
+    // получаю вузы
+    const { data, error, isLoading } = useSWR('/api/specs', fetcher)
+
+    // использую для получения айдишника чтобы можно было сделать edit fetch
+    const [dataId, setDataId] = useState('');
+
+    if (error) return <div>ошибка загрузки</div>
+    if (isLoading) return <div>загрузка...</div>
+
+    const addSpecialization = async () => {
+        try {
+            await axios.post('/api/spec', { name: inputValue });
+
+            // Тот самый mutate для того чтобы обновить данные
+            await mutate('/api/specs', fetcher('/api/specs'));
+
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const deleteSpecialization = async (id: string) => {
+        try {
+            await axios.delete(`/api/spec?id=${id}`);
+            await mutate('/api/specs', fetcher('/api/specs'));
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const editSpecialization = async (id: string) => {
+        try {
+            await axios.put('/api/spec', { id, name: newValue });
+            await mutate('/api/specs', fetcher('/api/specs'));
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+
+    const columns: ColumnDef<Spec>[] = [
         {
-            id: '1',
-            Spec: 'Программист',
+            header: "ID",
+            accessorKey: "id",
+        },
+        {
+            header: "Специальность",
+            accessorKey: "name",
+        },
+        {
+            id: "actions",
+            cell: ({ row }) => {
+                const spec = row.original
+                return (
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="ghost" className="h-8 w-8 p-0">
+                                <span className="sr-only">Open menu</span>
+                                <MoreHorizontal className="h-4 w-4" />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>Действия</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem onClick={() => {
+                                openModal();
+                                setNewValue(spec.name);
+                                setDataId(spec.id);
+                            }}>Изменить</DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => {
+                                deleteSpecialization(spec.id);
+                            }}>Удалить</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                )
+            },
         },
     ]
-}
-
-export default async function SpecsPage() {
-    const data = await getData();
     return (
         <div>
+            <Modal isOpen={isModalOpen} onClose={closeModal}>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Изменение специальности</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <Input value={newValue} onChange={(e) => setNewValue(e.target.value)} placeholder="Имя" type="text" />
+                    </CardContent>
+                    <CardFooter>
+                        <Button onClick={() => {
+                            editSpecialization(dataId)
+                            closeModal();
+                        }}>Изменить</Button>
+                    </CardFooter>
+                </Card>
+            </Modal>
             <p className="font-mono text-xl mb-4">
                 Специальности
             </p>
@@ -63,13 +138,13 @@ export default async function SpecsPage() {
                     <div className="w-full flex gap-2 items-end mb-2" >
                         <div className="grid w-full items-center gap-1.5">
                             <Label htmlFor="spec" className="mb-1">Название</Label>
-                            <Input id="spec" placeholder="Добавить специальность" type="text" />
+                            <Input id="spec" placeholder="Добавить специальность" type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
                         </div>
-                        <Button>Добавить</Button>
+                        <Button onClick={addSpecialization}>Добавить</Button>
                     </div>
                 </div>
             </div>
-            <DataTable columns={columns} data={data} />
+            <DataTable columns={columns} data={data.data} />
         </div>
     )
 }
--- src/app/api/spec/route.ts
+++ src/app/api/spec/route.ts
@@ -1,27 +1,59 @@
 import { NextResponse } from "next/server";
+import {v4 as uuid} from "uuid";
+import {deleteDoc, doc, getDoc, setDoc, updateDoc} from "firebase/firestore";
+import {db} from "../../../../firebase";
 
-export async function DELETE(req: Request, route: { params: { id: string } }) {
-  try {
-    return NextResponse.json({ comments: {}}, { status: 200 });
-  } catch (err) {
-    console.error(err);
-    return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
-  }
-}
-export async function POST(req: Request, route: { params: { id: string } }) {
+export async function GET(req: Request) {
     try {
-      return NextResponse.json({ comments: {}}, { status: 200 });
+        const { searchParams } = new URL(req.url);
+        const id = searchParams.get('id');
+        const ref = doc(db, "specs", id as string );
+        const res = await getDoc(ref).then(r => r.data());
+        return NextResponse.json({data: res}, { status: 200 });
     } catch (err) {
-      console.error(err);
-      return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
+        console.error(err);
+        return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
     }
 }
-
-export async function PUT(req: Request, route: { params: { id: string } }) {
+export async function POST(req: Request) {
     try {
-      return NextResponse.json({ comments: {}}, { status: 200 });
+        const body = await req.json();
+        const { name}  = body;
+        const specUID = uuid()
+        const ref = doc(db, "specs", specUID );
+        await setDoc(ref, {
+            id: specUID,
+            name: name,
+        });
+        return NextResponse.json( { status: 200 });
     } catch (err) {
-      console.error(err);
-      return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
+        console.error(err);
+        return NextResponse.json({ message: err }, { status: 500 });
+    }
+}
+export async function PUT(req: Request) {
+    try {
+        const body = await req.json();
+        const {id, name}  = body;
+        const ref = doc(db, "specs", id );
+        await updateDoc(ref, {
+            name: name
+        });
+        return NextResponse.json({ status: 200 });
+    } catch (err) {
+        console.error(err);
+        return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
+    }
+}
+export async function DELETE(req: Request) {
+    try {
+        const { searchParams } = new URL(req.url);
+        const id = searchParams.get('id');
+        const ref = doc(db, "specs", id as string );
+        await deleteDoc(ref);
+        return NextResponse.json({}, { status: 200 });
+    } catch (err) {
+        console.error(err);
+        return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
     }
 }
